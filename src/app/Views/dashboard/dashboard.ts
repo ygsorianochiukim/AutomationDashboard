aae327c4ec55d6591961a4c6bf773851f89179a0
glynn315 <94496602+glynn315@users.noreply.github.com>
2026-01-08 23:16:57 +0800

    Update Changes

--- src/app/Views/dashboard/dashboard.ts
+++ src/app/Views/dashboard/dashboard.ts
@@ -3,11 +3,13 @@ import { ConversationServices } from '../../Services/ConversationServices/conver
 import { Logs } from '../../Models/LogsList/logs.model';
 import { HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
+import { UpdateLogsList } from '../../Models/LogsList/update-logs-list.model';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [HttpClientModule, CommonModule],
+  imports: [HttpClientModule, CommonModule, FormsModule],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss',
   providers: [ ConversationServices ]
@@ -15,6 +17,13 @@ import { Router } from '@angular/router';
 export class Dashboard implements OnInit {
 
   LogsList: Logs[] = [];
+  UpdateLogField: UpdateLogsList = {
+    status: '',
+    transfer_count_bot: 10,
+    transfer_count_human:  10,
+    conversation_id:  null,
+  }
+  logsID: number = 0;
 
   constructor(private LogsServices : ConversationServices, private router : Router) {}
 
@@ -25,6 +34,18 @@ export class Dashboard implements OnInit {
   displayDashboard(){
     this.LogsServices.displayListLogsPending().subscribe((data) => {
       this.LogsList = data;
+    });
+  }
+
+  updateHandoff(log: any){
+    const payload = {
+      conversation_id: log.conversation_id,
+      status: log.status,
+      transfer_count_bot: log.transfer_count_bot,
+      transfer_count_human: log.transfer_count_human,
+    };
+    this.LogsServices.UpdateLogsList(payload).subscribe(() => {
+
     });
   }
 
